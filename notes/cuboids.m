1b3185310b15c702c869cf048e40d225b57fd91d
Ariel Nunez <ingenieroariel@gmail.com>
2013-11-13 17:08:14 -0500

    Added matlab code to generate cuboids

---
+++ notes/cuboids.m
@@ -0,0 +1,8 @@
+
+function save_file = generate_cuboids()
+    addpath(genpath('/home/x/toolbox/'));
+    addpath(genpath('/home/x/cuboids/'));
+
+    video_path='/home/x/Videos/D-ZBcmcje_s.avi';
+
+    save_file = stfeatures_long_video(video_path, '/home/x/Videos/output/');
