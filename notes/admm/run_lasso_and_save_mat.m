a24ee57e28e4347fe43cbbf84fdf3873feb90a8a
Ariel Nunez <ingenieroariel@gmail.com>
2013-12-19 10:11:32 -0500

    Updated notebook to use lasso_admm from lasso.py

--- notes/admm/run_lasso_and_save_mat.m
+++ notes/admm/run_lasso_and_save_mat.m
@@ -1,6 +1,6 @@
 A = randn(100,100);
 X = randn(100,80);
 tic;
-[B, cost, C_initial] = lasso_admm(X, A , 1);
+[B, cost] = lasso_admm(X, A , 1);
 toc;
-save('admm.mat', 'A', 'X', 'B', 'C_initial', 'cost')
+save('admm.mat', 'A', 'X', 'B', 'cost')
