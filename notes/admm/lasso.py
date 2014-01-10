2bea396a0fa32a4be051cdac7a572cdd2b54cd80
Ariel Nunez <ingenieroariel@gmail.com>
2014-01-10 10:44:22 -0500

    Successfully used lasso_admm as a solver for a simple equation

--- notes/admm/lasso.py
+++ notes/admm/lasso.py
@@ -2,10 +2,22 @@ import numpy as np
 from scipy import sparse as sp
 
 
+def lasso_cost(X, A, B, gamma):
+    """
+    Calculate the cost of the lasso function
+    """
+    norm2 = lambda x: np.dot(x.flatten(1).transpose(), x.flatten(1))
+    norm1 = lambda x: np.sum(np.abs(x.flatten(1)))
+    the_cost = 0.5 * norm2(X - np.dot(A, B)) + gamma * norm1(B)
+    return the_cost
+
+
 def lasso_admm(X, A, gamma=1, C=None, double=False, max_rho=5.0, rho=1e-4, max_iter=500):
-    # Cast to matrix if input is an array
+    # Cast to matrix if input is an array, for when A or X are simple arrays/lists/vectors
     if len(A.shape) == 1:
         A = A.reshape(A.shape[0],1)
+    if len(X.shape) == 1:
+        X = X.reshape(X.shape[0],1)
 
     c = X.shape[1]
     r = A.shape[1]
@@ -19,9 +31,6 @@ def lasso_admm(X, A, gamma=1, C=None, double=False, max_rho=5.0, rho=1e-4, max_i
 
     fast_sthresh = lambda x, th: np.sign(x) * np.maximum(np.abs(x) - th, 0)
 
-    norm2 = lambda x: np.dot(x.flatten(1).transpose(), x.flatten(1))
-    norm1 = lambda x: np.sum(np.abs(x.flatten(1)))
-
     cost = []
 
     for n in range(1, max_iter):
@@ -32,7 +41,7 @@ def lasso_admm(X, A, gamma=1, C=None, double=False, max_rho=5.0, rho=1e-4, max_i
 
         B,resid,rank,s = np.linalg.lstsq(F,G)
 
-        # Verify B is a solution to: F dot G = B
+        # Verify B is a solution to: F dot B = G
         np.testing.assert_array_almost_equal(np.dot(F, B), G)
 
         # Solve sub-problem to solve C
@@ -44,7 +53,7 @@ def lasso_admm(X, A, gamma=1, C=None, double=False, max_rho=5.0, rho=1e-4, max_i
         rho = min(max_rho, rho*1.1)
     
         # get the current cost
-        current_cost = 0.5 * norm2(X - np.dot(A, B)) + gamma * norm1(B)
+        current_cost = lasso_cost(X, A, B, gamma)
         cost.append(current_cost)
     
     return B, cost
--- notes/admm/test.py
+++ notes/admm/test.py
@@ -1,5 +1,5 @@
 import scipy.io as sio
-from lasso import lasso_admm
+from lasso import lasso_admm, lasso_cost
 import unittest
 import numpy as np
 
@@ -34,28 +34,49 @@ class TestSequenceFunctions(unittest.TestCase):
 
 
     def test_admm_solve(self):
+        """
+        Test lasso_admm works as a solver when gamma is zero
+        """
         # First let's define an equation of the form: X*B = A, where B is the unknown.
-        A = np.array([8,4])
-        X = np.array([[2,3], [5, -2]])
+        X = np.array([[8],[4]])
+        A = np.array([[2,3], [5, -2]])
         # Let's verify the solution obtained manually works
-        B_manual = np.array([ 1.47368421,  1.68421053])
-        np.testing.assert_array_almost_equal(A, np.dot(X, B_manual))
-
+        B_manual = np.array([[ 1.47368421],  [1.68421053]])
+        np.testing.assert_array_almost_equal(X, np.dot(A, B_manual))
 
         # Now let's solve it using lstsq
-        B_lstsq = np.linalg.lstsq(X, A)[0]
+        B_lstsq = np.linalg.lstsq(A, X)[0]
         # And compare with the manual solution
         np.testing.assert_array_almost_equal(B_lstsq, B_manual)
 
-        # Now let's use ADMM to solve the same equation
-        B_admm, cost = lasso_admm(X, A, gamma=1e-10)
+        # We drive gamma to zero to ignore the sparsity requirement of the problem
+        # and have it perform like a regular solver.
+        gamma = 0
+        # This an exact solution, the cost should be zero.
+        cost_expected = 0
 
-        # Is this an exact solution? The cost should be zero, right?
-        np.testing.assert_almost_equal(cost[-1], 0)
+        # Now let's use ADMM to solve the same equation
+        B_admm, cost_admm = lasso_admm(X, A, gamma=gamma)
+
+        # Let's evaluate the cost of the manual solution
+        cost_manual = lasso_cost(X, A, B_manual, gamma)
+
+        # Check the cost is what we expected
+        np.testing.assert_almost_equal(cost_manual, cost_expected)
 
         # And compare it with the B obtained manually
         np.testing.assert_array_almost_equal(B_admm, B_manual)
 
+        # Check the expected cost is also the result of lasso_admm
+        np.testing.assert_almost_equal(cost_admm[-1], cost_expected)
+
+        # Check the cost is higher when gamma is bigger than 0.
+        gamma = 1
+        B_admm2, cost_admm2 = lasso_admm(X, A, gamma=gamma)
+        cost_nonzero_gamma = cost_admm2[-1]
+
+        msg = "Expected cost higher than %s for gamma=%s, got %s" % (cost_expected, gamma, cost_nonzero_gamma)
+        assert cost_nonzero_gamma > cost_expected
 
 
 if __name__ == '__main__':
